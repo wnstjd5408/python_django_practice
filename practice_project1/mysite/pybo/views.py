886cae1ae4d69f790605adf5953b9ed1ffc9c146
wnstjd5408 <wnstjd5408@naver.com>
2021-07-23 02:10:20 +0900

    update pybo

--- practice_project1/mysite/pybo/views.py
+++ practice_project1/mysite/pybo/views.py
@@ -1,28 +1,33 @@
-# from django.http import HttpResponse
-# Create your views here.
-#----------------edit---------------------------#
-from .models import Question
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+
 from .forms import QuestionForm, AnswerForm
+from .models import Question
 
 
 def index(request):
     """
     pybo 목록 출력
     """
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+
+    # 조회
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
-# ---------------------------------- [edit] ---------------------------------- #
+
+    # 페이징처리
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
-# ---------------------------------------------------------------------------- #
 
 
 def detail(request, question_id):
     """
     pybo 내용 출력
     """
-
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
@@ -32,13 +37,7 @@ def answer_create(request, question_id):
     """
     pybo 답변등록
     """
-
     question = get_object_or_404(Question, pk=question_id)
-    #  answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    # answer.save()  이걸로 해도됨
-    # question.answer_set.create(
-    #     content=request.POST.get('content'), create_date=timezone.now())
-    # return redirect('pybo:detail', question_id=question.id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
