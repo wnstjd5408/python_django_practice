d61878f80902f529853245b14410b88d2c98755a
wnstjd5408 <wnstjd5408@naver.com>
2021-07-28 23:26:13 +0900

    3-07

--- practice_project1/mysite/pybo/views.py
+++ practice_project1/mysite/pybo/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 from .forms import QuestionForm, AnswerForm
 from .models import Question
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -33,15 +34,18 @@ def detail(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     """
     pybo 답변등록
     """
+
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # author  속성에 로그인 꼐정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -52,6 +56,7 @@ def answer_create(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def question_create(request):
     """
     pybo 질문등록
@@ -60,6 +65,7 @@ def question_create(request):
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user  # author 속성에 로그인 계정 저장
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
